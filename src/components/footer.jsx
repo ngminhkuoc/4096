8d641d61bda1dac236f4ca92a222cef9e8b04ece
Quoc Nguyen <quoc.nguyen@depotnet.co.uk>
2023-08-01 09:33:22 +0100

    update footer

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -9,7 +9,7 @@ export default class Footer extends Component {
     tiles with the same number touch, they <strong>merge into one!</strong>
                 </p>
                 <hr />
-                <p>Created by <a href="https://quocnguyen.dev" rel="noopener noreferrer" target="_blank">Quoc Nguyen</a>.&nbsp;
+                <p>Created by <a href="https://www.quocnguyen.dev" rel="noopener noreferrer" target="_blank">Quoc Nguyen</a>.&nbsp;
         A clone of <a href="https://github.com/gabrielecirulli/2048" rel="noopener noreferrer" target="_blank">2048</a>&nbsp;
         by <a href="http://gabrielecirulli.com" rel="noopener noreferrer" target="_blank">Gabriele Cirulli</a>&nbsp;
         written using <a href="https://reactjs.org/" target="_blank" rel="noopener noreferrer">React</a>.</p>
